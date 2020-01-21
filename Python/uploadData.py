23171e2af1b6bdafc734e759658032f06cf00d73
Misha Ward <wardm5@uw.edu>
2020-01-21 14:03:55 -0800

    test

---
+++ Python/uploadData.py
@@ -0,0 +1,33 @@
+# python imports
+import boto3
+from io import BytesIO
+import gzip
+
+
+import urllib2
+
+# url = 'http://winterolympicsmedals.com/medals.csv'
+
+
+
+
+
+# setup constants
+bucket = 'github-analysis-project'
+gzipped_key = 'http://ghtorrent-downloads.ewi.tudelft.nl/mongo-full/users-dump.2015-12-01.tar.gz'
+response = urllib2.urlopen(gzipped_key)
+uncompressed_key = 'data-file'
+
+# initialize s3 client, this is dependent upon your aws config being done
+s3 = boto3.client('s3', use_ssl=False)  # optional
+s3.upload_fileobj(                      # upload a new obj to s3
+    Fileobj=gzip.GzipFile(              # read in the output of gzip -d
+        None,                           # just return output as BytesIO
+        'rb',                           # read binary
+        fileobj=BytesIO(response),
+    Bucket=bucket,                      # target bucket, writing to
+    Key=uncompressed_key))               # target key, writing to
+
+# read the body of the s3 key object into a string to ensure download
+s = s3.get_object(Bucket=bucket, Key=gzip_key)['Body'].read()
+print(len(s))  # check to ensure some data was returned
--- bash_scripts/transfer_data.sh
+++ bash_scripts/transfer_data.sh
@@ -3,6 +3,25 @@
 # Misha Ward - 17/Jan/2020
 
 wget -O- "https://data.ok.gov/sites/default/files/unspsc%20codes_3.csv" | aws s3 cp - s3://github-analysis-project/data-file/testing.csv
+
+
 #  aws s3 cp s3://github-analysis-project/data-file/testing.tar.gz - | tar -xz  | aws s3 cp s3://github-analysis-project/data-file/
 
 #  tar -xz aws s3 cp s3://github-analysis-project/data-file/testing.tar.gz | aws s3 cp - s3://github-analysis-project/data-file/
+# tar -zxvf aws s3 cp s3://github-analysis-project/github.tar.gz
+# tar -zxvf aws s3 cp s3://github-analysis-project/github.tar.gz
+
+# aws s3 cp s3://github-analysis-project/github.tar.gz - | tar -xz | aws s3 cp - s3://github-analysis-project/folder
+# tar -zxvf
+
+wget -c "http://ghtorrent-downloads.ewi.tudelft.nl/mysql/mysql-2016-01-16.tar.gz" | tar -zxvf
+wget -c "http://ghtorrent-downloads.ewi.tudelft.nl/mongo-full/org_members-dump.2015-12-01.tar.gz" | tar -xz
+
+
+
+
+# wget -qO- "http://ghtorrent-downloads.ewi.tudelft.nl/mysql/mysql-2015-06-18.tar.gz" | tar -zxvf- | aws s3 cp - s3://github-analysis-project/data-file/testing123
+ tar -xzf "http://ghtorrent-downloads.ewi.tudelft.nl/mysql/mysql-2016-01-16.tar.gz"
+
+
+# wget -O- "https://data.ok.gov/sites/default/files/unspsc%20codes_3.csv" | aws s3 cp - s3://github-analysis-project/data-file/testing.csv
