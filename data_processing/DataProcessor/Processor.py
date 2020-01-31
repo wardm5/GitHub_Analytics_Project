6d68a4ea30c394ead2bb509f2fc2a178f1a99bfd
Misha Ward <wardm5@uw.edu>
2020-01-30 17:59:20 -0800

    added get table names method to Processor class

--- data_processing/DataProcessor/Processor.py
+++ data_processing/DataProcessor/Processor.py
@@ -11,11 +11,7 @@ class Processor():
         self.postgres_connector = Connector()   # writes tables to PostgreSQL
         self.s3_reader = Reader()               # reads tables from S3
         self.dic = {}
-        # self.commits = None         # table to store commit info
-        # self.users = None           # table to store user info
-        # self.projects = None        # table to store project info
         self.started = False        # flag to see if setup
-        # IDEA, make a map of name to dataframe for keeping track of tables...
 
     # Method to write tables to PostgreSQL database
     def write_to_postgres(self):
@@ -33,9 +29,6 @@ class Processor():
         self.dic['users'] = users
         self.dic['projects'] = projects
         self.dic['commits'] = commits
-        # self.users = self.s3_reader.read('users')
-        # self.projects = self.s3_reader.read('projects')
-        # self.commits = self.s3_reader.read('commits')
         self.started = True
 
     # Method to count all table rows
@@ -46,9 +39,6 @@ class Processor():
         print("commit count: " , self.dic.get('users').count())
         print("commit count: " , self.dic.get('projects').count())
         print("commit count: " , self.dic.get('commits').count())
-        # print("commit count: " , self.commits.count())
-        # print("users count: " , self.users.count())
-        # print("projects count: " , self.projects.count())
 
     # Method to get table stored in class
     def get_table(self, name):
@@ -56,16 +46,12 @@ class Processor():
             return
         if (self.dic.get(name) != None):
             return self.dic.get(name)
-        # elif (name == "commits"):
-        #     return self.commits
-        # elif (name == "users"):
-        #     return self.users
-        # elif (name == "projects"):
-        #     return self.projects
         else:
             "Incorrect table selected"
 
-    # get_table_names(self):
+    def get_table_names(self):
+        print("Tables include:  ", self.dic.keys())
+        return self.dic.keys()
 
     # Method to preprocess tables by removing unneeded columns
     def preprocess_tables(self):
@@ -84,29 +70,10 @@ class Processor():
         columns_to_drop_projects = ['forked_from', 'deleted']
         self.dic['projects'] = self.dic['projects'].drop(*columns_to_drop_projects)
 
-        # print("Status: dropping columns: commits")
-        # columns_to_drop_commits = ['sha', 'author_id']
-        # self.commits = self.commits.drop(*columns_to_drop_commits)
-        #
-        # print("Status: dropping columns: users")
-        # columns_to_drop_users = ['company', 'type', 'fake', 'long', 'lat']
-        # self.users = self.users.drop(*columns_to_drop_users)
-        #
-        # print("Status: dropping columns: projects")
-        # columns_to_drop_projects = ['forked_from', 'deleted']
-        # self.projects = self.projects.drop(*columns_to_drop_projects)
-
     # Method to show one specific table
     def show_table(self, name):
         if (self.dic.get(name) != None):
             self.dic.get(name).show()
-        # if (name == 'users'):
-        #
-        #     self.users.show()
-        # elif (name == 'projects'):
-        #     self.projects.show()
-        # elif (name == 'commits'):
-        #     self.commits.show()
         else:
             print("Incorrect table name, please try again")
 
