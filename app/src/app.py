a60d5c9bfcf513d19ca7c56c105220ead0160e22
Misha Ward <wardm5@uw.edu>
2020-02-01 18:47:28 -0800

    got dash app and flask api up and running

--- app/src/app.py
+++ app/src/app.py
@@ -1,18 +1,33 @@
-from dash import Dash
-import dash_core_components as dcc
+#****************************** Production Code ******************************#
+import flask
+import dash
+from main import *
 import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+import pandas as pd
 
-external_stylesheets = ['/static/dist/css/style.css']
-external_scripts = ['/static/dist/js/includes/jquery.min.js',
-                    '/static/dist/js/main.js']
+server = flask.Flask(__name__)
 
+# Restful API
+@server.route('/api/')
+def index():
+    return 'API functions:'
 
-app = Dash(__name__,
-          external_stylesheets=external_stylesheets,
-          external_scripts=external_scripts,
-          routes_pathname_prefix='/')
+# Dash App Functions
+app = dash.Dash(
+    __name__,
+    server=server,
+    routes_pathname_prefix='/'
+)
 
-app.layout = html.Div(id='example-div-element')
+df = run_query()
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+)
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,8 @@
 from PostgresReader.Reader import *
-
 sql = Reader()
+
+def run_query():
+    return sql.run_query("""SELECT * from default_2 where id = 5 """)
 
 sql.run_query("""SELECT * from default_2 where id = 5 """)
 sql.print_results()
