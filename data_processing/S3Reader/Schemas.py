d5c213023448846f8f329e03959b207f9514a22b
Misha Ward <wardm5@uw.edu>
2020-02-02 19:13:14 -0800

    updating schemas for extra data

--- data_processing/S3Reader/Schemas.py
+++ data_processing/S3Reader/Schemas.py
@@ -22,57 +22,90 @@ class Schemas():
     #**************************** Default schemas ****************************#
     # Method to return the commits schema
     def get_commits_schema(self):
-        commit_schema = StructType([
-            StructField("commit_id", IntegerType(), False),
+        schema = StructType([
+            StructField("id", IntegerType(), False),
             StructField("sha", StringType(), True),
             StructField("author_id", IntegerType(), True),
             StructField("committer_id", IntegerType(), True),
             StructField("project_id", IntegerType(), True),
             StructField("created_at", TimestampType(), True)
         ])
-        return commit_schema
+        return schema
 
-    # Method to return the users schema
-    def get_users_schema(self):
-        user_schema = StructType([
-            StructField("id", 	IntegerType(),False),	 	       #_C0
-            StructField("login",    StringType(),True), 		   #_c1
-            StructField("name",  StringType(),True), 		       #_c2
-            StructField("company", StringType(),True),             #_c3
-            StructField("location", 	StringType(),True),        #_c4
-            StructField("email", 	StringType(),True),            #_c5
-            StructField("created", 	TimestampType(),True),		   #_c6
-            StructField("type_of_user", 	StringType(),True),	   #_c7
-            StructField("fake", 	IntegerType(),True),		   #_c8
-            StructField("deleted", IntegerType(),True),		       #_c9
+    # Method to return the commit_comments schema
+    def get_commits_schema(self):
+        schema = StructType([
+            StructField("id", IntegerType(), True),
+            StructField("commit_id", IntegerType(), True),
+            StructField("user_id", IntegerType(), True),
+            StructField("body", StringType(), True),
+            StructField("line", IntegerType(), True),
+            StructField("position", IntegerType(), True),
+            StructField("comment_id", IntegerType(), True),
+            StructField("created_at", TimestampType(), True)
         ])
-        return user_schema
+        return schema
+
+    # Method to return followers schema
+    def get_followers_schema(self):
+        schema = StructType([
+            StructField("user_id", IntegerType(), True),
+            StructField("follower_id", IntegerType(), True),
+            StructField("created_at", TimestampType(), True)
+        ])
+        return schema
 
     # Method to return the projects schema
     def get_projects_schema(self):
-        project_schema = StructType([
-            StructField("id", 	        IntegerType(),False),	 	#_C0
-            StructField("url", 	        StringType(),True),	 	    #_C1
-            StructField("owner_id", 	IntegerType(),True),	 	#_C2
-            StructField("name", 	    StringType(),True),	 	    #_C3
-            StructField("description", 	StringType(),True),	 	    #_C4
-            StructField("language", 	StringType(),True),	 	    #_C5
-            StructField("created_at", 	TimestampType(),True),	 	#_C6
-            StructField("forked_from", 	IntegerType(),True),	 	#_C7
-            StructField("deleted", 	    BooleanType(),True),	 	#_C8
-            StructField("updated_at", 	TimestampType(),True),	 	#_C9
+        schema = StructType([
+            StructField("project_id", 	  IntegerType(),False),	 	#_C0
+            StructField("language", 	  StringType(),True),	 	#_C1
+            StructField("bytes", 	      IntegerType(),True),	 	#_C2
+            StructField("created_at", 	  TimestampType(),True)     #_C3
         ])
-        return project_schema
+        return schema
+
+    # Method to return the projects schema
+    def get_projects_schema(self):
+        schema = StructType([
+            StructField("id", 	                IntegerType(),False),	 	#_C0
+            StructField("url", 	                StringType(),True),	 	    #_C1
+            StructField("owner_id", 	        IntegerType(),True),	 	#_C2
+            StructField("name", 	            StringType(),True),	 	    #_C3
+            StructField("description", 	        StringType(),True),	 	    #_C4
+            StructField("language", 	        StringType(),True),	 	    #_C5
+            StructField("created_at", 	        TimestampType(),True),	 	#_C6
+            StructField("forked_from", 	        IntegerType(),True),	 	#_C7
+            StructField("deleted", 	            BooleanType(),True),	 	#_C8
+            StructField("updated_at", 	        TimestampType(),True),      #_C9
+            StructField("forked_commit_id", 	IntegerType(),True)	 	    #_C10
+        ])
+        return schema
 
     # Method to return the repo_lables schema
     def get_repo_label_schema(self):
-        repo_label_schema = StructType([
+        schema = StructType([
             StructField("id", 	        IntegerType(),False),	 	#_C0
             StructField("repo_id", 	    IntegerType(),True),	 	#_C1
             StructField("name", 	    StringType(),True),	 	    #_C2
         ])
-        return repo_label_schema
+        return schema
 
-    # Method to return followers schema
-    def get_followers_schema(self):
-        return None
+    # Method to return the users schema
+    def get_users_schema(self):
+        schema = StructType([
+            StructField("id", 	     IntegerType(),False),	 	       #_C0
+            StructField("login",     StringType(),True), 		   #_c1
+            StructField("company",   StringType(),True), 		       #_c2
+            StructField("created_at", TimestampType(),True),             #_c3
+            StructField("type", 	 StringType(),True),        #_c4
+            StructField("fake", 	 IntegerType(),True),            #_c5
+            StructField("deleted", 	 IntegerType(),True),		   #_c6
+            StructField("long", 	 DecimalType(),True),	   #_c7
+            StructField("lat", 	     DecimalType(),True),		   #_c8
+            StructField("country_code", StringType(),True),
+            StructField("state",     StringType(),True),
+            StructField("city",      StringType(),True),		       #_c9
+            StructField("location",  StringType(),True)
+        ])
+        return schema
