82c11bc636f5f5eae81a2d71b0b53555fb01a36d
Misha Ward <wardm5@uw.edu>
2020-02-06 14:44:09 -0800

    added method for timestamp/log reporting

--- data_processing/DataProcessor/PostgresConnector/Connector.py
+++ data_processing/DataProcessor/PostgresConnector/Connector.py
@@ -1,3 +1,4 @@
+import time, datetime
 # Class to connect to PostgreSQL database
 class Connector():
     # Constructor for class, sets the database, url for connection, and properties needed for connection
@@ -7,7 +8,13 @@ class Connector():
         self.properties = {"user": db_user, "password": password, "driver": "org.postgresql.Driver"}
 
     # Write method for connection, will write to the PostgreSQL database
-    def write(self, data_frame, mode, name):
+    def write(self, data_frame, mode, name, timer):
         print("Status: writing to database... ", name)
         data_frame.write.jdbc(url=self.url, table=name, mode=mode, properties=self.properties)
         print("Status: COMPLETE")
+        self.end_timestamp(timer, name)
+
+    # Method to end timestamp, records to log
+    def end_timestamp(self, timer, name):
+        with open("/home/ubuntu/data_processing/log/log.txt", "a") as myfile:
+            myfile.write('WRITE TO DATABASE COMPLETED: ' + str(datetime.datetime.now()) + ',TOTAL RUN TIME: ' + str(int(time.time() - timer)) + ' seconds, METHOD: ' + name + '\n')
